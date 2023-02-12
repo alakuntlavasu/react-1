85fde4eb0e648bdc9ff71b225c0682f83c6b40f5
alakuntlavasu <alakuntlavasu2020@gmail.com>
2023-02-12 22:36:58 +0530

    okk

--- src/List rendaring react/List2.js
+++ src/List rendaring react/List2.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 function List2() {
-    const names=['Apple','banana','orange']
+  const names = ['Apple', 'banana', 'orange']
   return (
     <div>
-    <h1>{names[0]}</h1>
-    <h2>{names[1]}</h2>
-    <h3>{names[2]}</h3>
+      <h1>{names[0]}</h1>
+      <h2>{names[1]}</h2>
+      <h3>{names[2]}</h3>
 
     </div>
   )
